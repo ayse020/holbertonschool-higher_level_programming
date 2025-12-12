b6a903d623a545ac27d2f75d06bf6b9793195d8d
Ayse <ayse@example.com>
2025-12-12 17:09:36 +0000

    Task 3: Common elements

---
+++ 3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    
+    Args:
+        set_1: First set
+        set_2: Second set
+    
+    Returns:
+        Set of common elements
+    """
+    return set_1 & set_2

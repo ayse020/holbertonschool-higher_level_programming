67d69a6e73c537cf1612c3e4fe371ec98715dca1
Ayse <ayse@example.com>
2025-12-12 18:55:50 +0000

    Add 6-print_sorted_dictionary.py function

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    """Print a dictionary by ordered keys."""
+    # Get sorted keys
+    sorted_keys = sorted(a_dictionary.keys())
+    
+    # Print each key-value pair
+    for key in sorted_keys:
+        print("{}: {}".format(key, a_dictionary[key]))

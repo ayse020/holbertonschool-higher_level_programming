fd8f7d722f9369b7c273c07eeacab3755758ae6d
Ayse <ayse@example.com>
2025-12-12 12:46:43 +0000

    Fix pycodestyle: remove whitespace from blank lines

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     """
     Prints a matrix of integers.
-    
+
     Args:
         matrix: A list of lists containing integers
                 Default is an empty matrix [[]]
@@ -11,7 +11,7 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(row)):
             # Use str.format() to print integer without casting to string
             print("{:d}".format(row[i]), end="")
-            
+
             # Add space between elements, but not after the last one
             if i != len(row) - 1:
                 print(" ", end="")

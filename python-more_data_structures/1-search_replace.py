b377be564d8191694194ae71b7cedf1df95299d0
Ayse <ayse@example.com>
2025-12-12 18:19:15 +0000

    Fix: Task 1 Pycodestyle - remove whitespace from lines 6 and 11

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,12 +3,12 @@
 def search_replace(my_list, search, replace):
     """
     Replaces all occurrences of an element in a list.
-    
+
     Args:
         my_list: The initial list
         search: The element to replace
         replace: The new element
-    
+
     Returns:
         A new list with all occurrences of 'search' replaced by 'replace'
     """

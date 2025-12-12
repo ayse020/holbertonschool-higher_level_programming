8ef4a268c9c5c39ae456df3128d4f2720bee6004
Ayse <ayse@example.com>
2025-12-12 18:10:28 +0000

    Fix: Pycodestyle whitespace errors (W293) on lines 6 and 9

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,10 @@
 def square_matrix_simple(matrix=[]):
     """
     Computes the square value of all integers in a matrix.
-    
+
     Args:
         matrix: A 2D list of integers
-    
+
     Returns:
         A new matrix with same size where each value is squared
     """

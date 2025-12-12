38666c009aa7d831beffbc4f4ba293c319d85b17
Ayse <ayse@example.com>
2025-12-12 17:07:15 +0000

    Task 2: Unique addition

---
+++ 2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list.
+    
+    Args:
+        my_list: List of integers
+    
+    Returns:
+        Sum of all unique integers
+    """
+    return sum(set(my_list))

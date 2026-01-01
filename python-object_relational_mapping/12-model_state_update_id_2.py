642d027befd650c88aaa803d12c5e8479cb2cbc3
Ayse <ayse@example.com>
2026-01-01 14:57:24 +0000

    Finalize 12-model_state_update_id_2.py with PEP 8 compliance

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Skript: State obyektinin adını yeniləmək
-Məqsəd: id-si 2 olan State obyektinin adını "New Mexico" olaraq dəyişdirmək
+Script that changes the name of a State object from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -10,44 +9,29 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # 1. Arqumentləri oxumaq
     if len(sys.argv) != 4:
-        print("İstifadə: ./12-model_state_update_id_2.py <mysql username> <mysql password> <database name>")
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
-    # 2. MySQL server-ə qoşulmaq
-    # .format() metodu ilə - daha uyğun format
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
     )
-    
-    # 3. Session yaratmaq
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    try:
-        # 4. id-si 2 olan State obyektini tapmaq
-        state_to_update = session.query(State).filter(State.id == 2).first()
-        
-        # 5. Əgər obyekt tapsa, onu yeniləmək
-        if state_to_update:
-            state_to_update.name = "New Mexico"
-            session.commit()
-            print("State obyekti uğurla yeniləndi!")
-        else:
-            print("Xəta: id-si 2 olan State obyekti tapılmadı")
-    
-    except Exception as e:
-        print(f"Xəta baş verdi: {e}")
-        session.rollback()
-    
-    finally:
-        # 6. Session-u bağlamaq
-        session.close()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()

02b9f485d0cc1e286cc46b6dd66ec4343682ca9c
Ayse <ayse@example.com>
2026-01-05 15:03:08 +0000

    Complete task 05: The Mystical Dragon - Implementing mixins with SwimMixin and FlyMixin

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
-"""Dragon with Mixins"""
+"""Dragon sinifi - Mixin nümunəsi"""
+
 
 class SwimMixin:
-    """Mixin for swimming capability"""
-    
+    """Üzmək qabiliyyəti təmin edən mixin"""
     def swim(self):
-        """Return swimming message"""
-        return "The creature swims!"
+        print("The creature swims!")
+
 
 class FlyMixin:
-    """Mixin for flying capability"""
-    
+    """Uçmaq qabiliyyəti təmin edən mixin"""
     def fly(self):
-        """Return flying message"""
-        return "The creature flies!"
+        print("The creature flies!")
+
 
 class Dragon(SwimMixin, FlyMixin):
-    """Dragon class with swimming and flying capabilities"""
-    
+    """Həm üzə bilən, həm də uça bilən əjdaha"""
     def roar(self):
-        """Dragon's roar"""
-        return "The dragon roars!"
-    
-    def breath_fire(self):
-        """Dragon's fire breath"""
-        return "The dragon breathes fire!"
+        print("The dragon roars!")

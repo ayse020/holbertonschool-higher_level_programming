9e41ac00c1f33e3cebcbd3b3ced59e11b377a021
Ayse <ayse@example.com>
2026-01-05 15:46:00 +0000

    Task 3: Complete to_json_string function with PEP8 compliance

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""3-to_json_string.py module."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return JSON representation of an object."""
+    return json.dumps(my_obj)

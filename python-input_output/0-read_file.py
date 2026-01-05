b12921c53aa270114c0210dbb4d9c7a1e864d8ea
Ayse <ayse@example.com>
2026-01-05 15:15:39 +0000

    Fix PEP8: remove trailing whitespace from blank lines

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,13 +4,14 @@ Module: 0-read_file.py
 Contains function that reads a text file and prints it to stdout.
 """
 
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
-    
+
     Args:
         filename (str): The name/path of the file to read.
-    
+
     Returns:
         None
     """

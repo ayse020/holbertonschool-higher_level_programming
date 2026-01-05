df5ea624250b8c26b9e49682784239e6233f7aaa
Ayse <ayse@example.com>
2026-01-05 13:46:28 +0000

    Update README.md and complete task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
-    
+    new_list = []
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -16,6 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
-            result_list.append(result)
-    
-    return result_list
+            new_list.append(result)
+    return new_list
--- python-exceptions/README.md
+++ python-exceptions/README.md
@@ -4,7 +4,7 @@ This project contains Python scripts for exception handling exercises.
 
 ## Files
 - 0-safe_print_list.py: Safe list printing function
-
 - 1-safe_print_integer.py: Safe integer printing function
-
 - 2-safe_print_list_integers.py: Print and count integers function
+- 3-safe_print_division.py: Safe division function
+- 4-list_division.py: Divide element by element function

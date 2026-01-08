59ba9e0e29dcd6592195131e2d9e56fa44a754fc
Ayse <ayse@example.com>
2026-01-08 12:45:27 +0000

    Add 0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Script to list all privileges for MySQL users user_0d_1 and user_0d_2
+-- Shows grants for user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Shows grants for user_0d_2@localhost  
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

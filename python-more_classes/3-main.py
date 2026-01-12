38aca7a58f732d20a43a2f8d9eec5447f15df1b1
Ayse <ayse@example.com>
2026-01-12 18:43:30 +0000

    3. Rectangle class with __str__ and __repr__, PEP8 clean

--- python-more_classes/3-main.py
+++ python-more_classes/3-main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Test file for Rectangle class."""
+from rectangle import Rectangle
 
-Rectangle = __import__('3-rectangle').Rectangle
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(),
-                                        my_rectangle.perimeter()))
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(str(my_rectangle))
-print(repr(my_rectangle))
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(
+        "Area:", my_rectangle.area(),
+        "- Perimeter:", my_rectangle.perimeter()
+    )
+    print(my_rectangle)
+    print("--")
+    print("##########")
+    print("##########")
+    print("##########")
+    another_rectangle = Rectangle(10, 3)
+    print(another_rectangle)
